bad3866d7d56aefa2ae76d6754a69bd814deabbc
Kowalski Dragon (kowalski7cc) <kowalski7cc@users.noreply.github.com>
2023-11-30 20:24:44 +0100

    Update tooling
    Signed-off-by: Kowalski Dragon (kowalski7cc) <kowalski7cc@users.noreply.github.com>

--- get_day.py
+++ get_day.py
@@ -1,4 +1,5 @@
 #!/bin/env python3
+import textwrap
 import datetime
 import argparse
 import logging
@@ -7,11 +8,33 @@ import os
 import dotenv
 import html2text
 import requests
-import nbformat as nbf
 from lxml import html
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
+
+package_json = """{
+    "name": "aoc-2022",
+    "version": "1.0.0",
+    "description": "",
+    "main": "solution.js",
+    "scripts": {
+        "build": "tsc",
+        "start": "node solution.js"
+    },
+    "keywords": [],
+    "devDependencies": {
+    "@types/node": "^20.10.1"
+  }
+}"""
+
+tsconfig_json = """{
+    "compilerOptions": {
+        "types": [
+            "node"
+        ]
+    },
+}"""
 
 
 def main():
@@ -23,6 +46,7 @@ def main():
     args = argparse.ArgumentParser(parents=[dateparser])
     args.add_argument("-d", "--day", default=today.day, required=deciding_args.year)
     dateparser.set_defaults(year=today.year)
+    args.add_argument("-l", "--language", default="python")
     opts = args.parse_args()
 
     year: int = opts.year
@@ -77,16 +101,46 @@ def main():
             file.write(chunk)
         log.info("Input downloaded")
 
-    notebook_path = os.path.join(day_root, "solution.ipynb")
-    if not os.path.exists(notebook_path):
-        new_nb = nbf.v4.new_notebook()
-        new_nb["cells"] = [
-            nbf.v4.new_code_cell(
-                'with open("sample-1.txt") as f:\n\traw = f.read().splitlines()'
-            ),
-            nbf.v4.new_code_cell(""),
-        ]
-        nbf.write(new_nb, notebook_path)
+    if opts.language in ["python", "py", "py3", "p"]:
+        import nbformat as nbf
+
+        notebook_path = os.path.join(day_root, "solution.ipynb")
+        if not os.path.exists(notebook_path):
+            new_nb = nbf.v4.new_notebook()
+            new_nb["cells"] = [
+                nbf.v4.new_code_cell(
+                    'with open("sample-1.txt") as f:\n\traw = f.read().splitlines()'
+                ),
+                nbf.v4.new_code_cell(""),
+            ]
+            nbf.write(new_nb, notebook_path)
+
+    if opts.language in ["typescript", "ts", "t"]:
+        # # create package.json
+        # with open(
+        #     os.path.join(day_root, "package.json"), "w", encoding="utf-8"
+        # ) as file:
+        #     file.write(package_json)
+
+        import subprocess
+
+        subprocess.run(["npm", "install" , "-D", "@types/node"], cwd=day_root)
+
+        # create tsconfig.json
+        with open(
+            os.path.join(day_root, "tsconfig.json"), "w", encoding="utf-8"
+        ) as file:
+            file.write(tsconfig_json)
+
+        with open(os.path.join(day_root, "solution.ts"), "w", encoding="utf-8") as file:
+            file.write(textwrap.dedent(
+                """\
+                import * as fs from 'fs';
+                import * as path from 'path';
+                import * as util from 'util';
+
+                const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf-8');"""
+            ))
 
 
 if __name__ == "__main__":
